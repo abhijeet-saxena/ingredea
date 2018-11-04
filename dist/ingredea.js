c6225a76490205059884c1e0f4142b938a48e46e
Abhijeet Saxena <saxenaabhijeet@outlook.com>
2018-11-04 21:51:58 +0530

    Adding Intelligent dish scan feature

--- dist/ingredea.js
+++ dist/ingredea.js
@@ -63,22 +63,60 @@ function generateUserDishCard(url, data) {
       additionalIngredients.push(ingredient.name);
     else maybeContains.push(ingredient.name);
   });
-
+  var topIngredients = "";
+  
   mainIngredients.forEach((item, i) => {
-    if (i == 1)
-      document.getElementById("mainI").innerHTML = "Main Ingredients:\n";
+    if (i == 0)
+      document.getElementById("mainI").innerHTML = "<b>Main Ingredients:</b>\n";
+    if(i < 5)  topIngredients += item+"," 
     if (i < 10)
       document.getElementById("mainI").innerHTML += `<li>${item}</li>`;
   });
   additionalIngredients.forEach((item, i) => {
-    if (i == 1)
-      document.getElementById("addI").innerHTML = "Additional Ingredients: \n";
+    if (i == 0)
+      document.getElementById("addI").innerHTML = "<b>Additional Ingredients: </b>\n";
     if (i < 10) document.getElementById("addI").innerHTML += `<li>${item}</li>`;
   });
   maybeContains.forEach((item, i) => {
-    if (i == 1)
-      document.getElementById("maybeI").innerHTML = "May also contain: \n";
+    if (i == 0)
+      document.getElementById("maybeI").innerHTML = "<b>May also contain: </b>\n";
     if (i < 10)
       document.getElementById("maybeI").innerHTML += `<li>${item}</li>`;
   });
+  topIngredients = topIngredients.replace(" ","%20");
+  topIngredients = topIngredients.slice(0,topIngredients.length-1);
+  generateSuggestions(topIngredients);
+}
+
+function generateSuggestions(searchString){
+  fetch(`https://api.edamam.com/search?q=${searchString}&app_id=4485101a&app_key=94a4dfd20a876aa86be092aa4c145667&from=0&to=5`)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    var results = myJson.hits;
+    var recipes = [];
+    document.getElementById('user-suggestions').innerHTML = ``;
+ 
+results.forEach(item =>{
+  recipes.push({
+    "dish_name": item.recipe.label,
+    "recipe_address": item.recipe.url,
+    "recipe_source": item.recipe.source,
+    "diet_labels": item.recipe.dietLabels,
+    "health_labels": item.recipe.healthLabels,
+  });
+
+document.getElementById('user-suggestions').innerHTML += `
+<ul> <h3>${item.recipe.label}</h3>
+<li>Full Recipe at <a href="${item.recipe.url}">${item.recipe.url}</a></li>
+<li>Recipe source: ${item.recipe.source}</li>
+<li>Diet Labels:  ${item.recipe.dietLabels}</li>
+<li>Health Labels: ${item.recipe.healthLabels}</li>
+</ul>
+`;
+
+});
+console.log(recipes);
+})
 }

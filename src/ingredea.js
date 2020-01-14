d5aee67b30f3e08a3ae0e790571c7707ab087888
Abhijeet Saxena <saxenaabhijeet@outlook.com>
2020-01-15 00:00:00 +0530

    Remove vulnerabilities

---
+++ src/ingredea.js
@@ -0,0 +1,83 @@
+var uploadImageToClarafai = (type) => {
+  var url = "";
+  var base64url = "";
+  const app = new Clarifai.App({
+    apiKey: "f577985bc79a416c87965f5d6d32f13b",
+  });
+
+  if (type === "image") {
+    var file = document.getElementById("user-img").files[0];
+    var reader = new FileReader();
+    if (file) {
+      reader.readAsDataURL(file);
+      reader.addEventListener(
+        "load",
+        function () {
+          url = reader.result;
+          base64url = reader.result.replace(/^data:image\/(.*);base64,/, "");
+          // predict the contents of an image by passing in a url
+          app.models.predict(Clarifai.FOOD_MODEL, base64url).then(
+            function (response) {
+              generateUserDishCard(url, response.outputs[0].data.concepts);
+            },
+            function (err) {
+              console.error(err);
+            }
+          );
+        },
+        false
+      );
+    } else {
+      alert("No Image Uploaded");
+    }
+  } else {
+    url = document.getElementById("user-img-url").value;
+    if (url !== "") {
+      // predict the contents of an image by passing in a url
+      app.models.predict(Clarifai.FOOD_MODEL, url).then(
+        function (response) {
+          generateUserDishCard(url, response.outputs[0].data.concepts);
+        },
+        function (err) {
+          console.error(err);
+        }
+      );
+    } else alert("Enter A URL");
+  }
+};
+
+function generateUserDishCard(url, data) {
+  var userCard = document.getElementById("user-dish-card");
+  userCard.style.visibility = "visible";
+  var dish_img = document.getElementById("dish-img");
+  dish_img.src = url;
+
+  var mainIngredients = [];
+  var additionalIngredients = [];
+  var maybeContains = [];
+
+  Array.from(data).forEach((ingredient) => {
+    if (ingredient.value > 0.65) mainIngredients.push(ingredient.name);
+    else if (ingredient.value > 0.35)
+      additionalIngredients.push(ingredient.name);
+    else maybeContains.push(ingredient.name);
+  });
+
+  mainIngredients.forEach((item, i) => {
+    if (i == 1)
+      document.getElementById("mainI").innerHTML = "Main Ingredients:\n";
+    if (i < 10)
+      document.getElementById("mainI").innerHTML += `<li>${item}</li>`;
+  });
+  additionalIngredients.forEach((item, i) => {
+    if (i == 1)
+      document.getElementById("addI").innerHTML = "Additional Ingredients: \n";
+    if (i < 10) document.getElementById("addI").innerHTML += `<li>${item}</li>`;
+  });
+  maybeContains.forEach((item, i) => {
+    if (i == 1)
+      document.getElementById("maybeI").innerHTML = "May also contain: \n";
+    if (i < 10)
+      document.getElementById("maybeI").innerHTML += `<li>${item}</li>`;
+  });
+}
---
+++ src/main.js
@@ -0,0 +1,67 @@
+var login = () => {
+  var uname = document.getElementById("uname").value;
+  var pwd = document.getElementById("pwd").value;
+  var q = faunadb.query;
+  var client = new faunadb.Client({
+    secret: "fnAC-5BzmsACCMj3RTsPxdDfLxJ26nM9INSiboaA"
+  });
+
+  client
+    .query(
+      q.Paginate(
+        q.Match(q.Index("authenticate_user"), [uname.toLowerCase(), pwd])
+      )
+    )
+    .then(ret => {
+      if (ret.data.length) {
+        const todoRefs = ret.data[0];
+        var vals = Object.values(todoRefs);
+        var reference = Object.values(vals)[0].split("/")[2];
+        sessionStorage.setItem("isLoggedIn", "true");
+        client.query(q.Get(q.Ref(q.Class("users"), reference))).then(ret => {
+          sessionStorage.setItem("loggedInUser", JSON.stringify(ret.data));
+          self.location = "./home.html";
+        });
+      } else {
+        alert("Invalid credentials");
+      }
+    });
+
+  return false;
+};
+
+var register = () => {
+  var fname = document.getElementById("signup_fname").value;
+  var lname = document.getElementById("signup_lname").value;
+  var email = document.getElementById("email").value;
+  var pwd = document.getElementById("signup_pwd").value;
+
+  var q = faunadb.query;
+
+  var client = new faunadb.Client({
+    secret: "fnAC-5BzmsACCMj3RTsPxdDfLxJ26nM9INSiboaA"
+  });
+
+  client
+    .query(
+      q.Create(q.Class("users"), {
+        data: {
+          fname: fname,
+          lname: lname,
+          email: email,
+          pwd: pwd
+        }
+      })
+    )
+    .then(ret => {
+      console.log(ret);
+      sessionStorage.setItem("isLoggedIn", "true");
+      sessionStorage.setItem("loggedInUser", JSON.stringify(ret.data));
+      self.location = "./home.html";
+    })
+    .catch(err => {
+      console.log(err);
+      alert("You did something wrong ");
+    });
+  return false;
+};
